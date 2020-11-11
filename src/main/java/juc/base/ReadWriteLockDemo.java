954e7ac0320a4833330a5041f7638e7676ca8327
zhouzibo <18208965365@163.com>
2020-11-11 20:13:17 +0800

    读写锁

---
+++ src/main/java/juc/base/ReadWriteLockDemo.java
@@ -0,0 +1,68 @@
+package juc.base;
+
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+public class ReadWriteLockDemo {
+
+    static class ReadWriteObj {
+        static ReadWriteLock readWriteLock;
+        static Lock readLock;
+        static Lock writeLock;
+        static String str = "";
+        static {
+            readWriteLock = new ReentrantReadWriteLock();
+            readLock = readWriteLock.readLock();
+            writeLock = readWriteLock.writeLock();
+        }
+
+        public static void readStr() {
+            try{
+                readLock.lock();
+                System.out.println(Thread.currentThread().getName() + "开始读数据");
+                Thread.sleep(2000);
+                System.out.println(Thread.currentThread().getName() + ":" + str);
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                readLock.unlock();
+            }
+        }
+
+        public static void writeStr(String data) {
+            try{
+                writeLock.lock();
+                System.out.println(Thread.currentThread().getName() + "开始写数据");
+                Thread.sleep(5000);
+                str = data;
+                System.out.println(Thread.currentThread().getName() + "写数据完成");
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                writeLock.unlock();
+            }
+        }
+
+
+    }
+
+    public static void main(String[] args) {
+        new Thread(() -> {
+            ReadWriteObj.writeStr("10");
+        }).start();
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        // 10个线程读数据
+        for (int i = 0; i < 10; i++) {
+            new Thread(() -> {
+                ReadWriteObj.readStr();
+            }).start();
+        }
+
+    }
+}
